11abdc9c289d7efce38a9dcf536ebb4b0e1184f7
sayersb <bradleysayers1@gmail.com>
2018-07-31 18:08:59 +0100

    initial commit and webpack set up

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-erofhijfh
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.use(express.static(`${__dirname}/public`));
+
+
+
+app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+app.listen(port, () => console.log(`Express running on port ${port}`));
+
+module.exports = app;
