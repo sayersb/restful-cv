0397282ff5b2e40d991d500e83e505f025f7a798
sayersb <bradleysayers1@gmail.com>
2018-08-21 15:55:41 +0100

    show page updated to show project repository link, status and technologies used

--- db/seeds.js
+++ db/seeds.js
@@ -12,7 +12,7 @@ mongoose.connect(dbURI, (err, db) => {
       type: 'Game',
       startDate: '2018-05-15',
       status: 'Complete',
-      technologies: ['HTML5', 'CSS3', 'JavaScript', 'jQuery'],
+      technologies: ['HTML5, ', 'CSS3, ', 'JavaScript, ', 'jQuery'],
       image: 'https://github.com/sayersb/project-1-wdi/raw/master/images/chain1.gif',
       description: 'For my first project, I had one week to devise a game concept then build to completion. This is a memory based game that utilised HTML5, CSS3, SASS, JavaScript and jQuery. The computer generates a random sequence to light up the cells in order, the user then has to repeat the same order to progress to the next level and increase the sequence length by one.',
       projectRepo: 'https://github.com/sayersb/project-1-wdi',
--- src/components/projects/Show.js
+++ src/components/projects/Show.js
@@ -33,6 +33,15 @@ class ProjectsShow extends React.Component {
 
         <div className="column is-half">
           <h2 className="title">{this.state.project.name}</h2>
+          <hr />
+          <h2 className="subtitle">{this.state.project.description}</h2>
+          <hr />
+          <h2 className="subtitle"><strong>Technologies Used :</strong> {this.state.project.technologies}</h2>
+          <hr />
+          <h2 className="subtitle"><strong>Status : </strong>{this.state.project.status}</h2>
+          <h2 className="subtitle">{this.state.project.team}</h2>
+          <hr />
+          <h2 className="subtitle">Project Repository Link : {this.state.project.projectRepo}</h2>
 
           <hr />
 
