f0b2e9df00081b5fbf2c03cf8022f1717fbefa40
sayersb <bradleysayers1@gmail.com>
2018-08-22 12:44:23 +0100

    second project seed added to site

--- db/seeds.js
+++ db/seeds.js
@@ -17,6 +17,16 @@ mongoose.connect(dbURI, (err, db) => {
       description: 'For my first project, I had one week to devise a game concept then build to completion. This is a memory based game that utilised HTML5, CSS3, SASS, JavaScript and jQuery. The computer generates a random sequence to light up the cells in order, the user then has to repeat the same order to progress to the next level and increase the sequence length by one.',
       projectRepo: 'https://github.com/sayersb/project-1-wdi',
       team: 'Solo Project'
+    }, {
+      name: 'World Cup Predictor',
+      type: 'Web App',
+      startDate: '2018-05-22',
+      status: 'Complete',
+      technologies: ['HTML5, ', 'CSS3, ', 'JavaScript, ', 'Node.js, ', 'Express, ', 'Yarn, ', 'bcrypt, ', 'body-parser, ', 'EJS, ', 'Session Authentication, ', 'method-override, ', 'MongoDB, ', 'Mongoose, ', 'Morgan '],
+      image: 'https://github.com/sayersb/project-1-wdi/raw/master/images/chain1.gif',
+      description: 'One week to create an Express, RESTful app setting out information on teams & players leading to predictions. Included Express, MongoDB and EJS in addition to the previous technologies utilised.',
+      projectRepo: 'https://github.com/sayersb/project-1-wdi   https://worldcuppredictor.herokuapp.com',
+      team: 'Solo Project'
     }]))
     .then(projects => console.log(`${projects.length} projects created`))
     .catch(err => console.log(err))
