6f6ba6a2c1c77b448b1719affa35a84d73091b36
sayersb <bradleysayers1@gmail.com>
2018-08-27 17:44:41 +0100

    about me updated and body background for app set to darkgrey

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 const About = () => {
   return (
-    <h1> About Page </h1>
+    <section className="hero is-medium is-dark is-bold">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">
+            About Me
+          </h1>
+          <hr />
+          <h2 className="subtitle">
+            Always had a passion for technology, cemented after receiving my first laptop and iPod after a serious accident aged 11. At University studying Economics and Social Studies, I confirmed the idea that I enjoyed problem solving challenges, with an ideal solution of helping people out. During this time, I received a compensation for the accident that I used to start a property company with my parents.
+
+            Having found, purchased, renovated and now continuing to manage the properties, I am in a position where I can afford to take up a new challenge and career. I love being kept on my toes and solving problems, so after seeing a friend start his journey in the web development industry a couple of years ago, I enquired further and found that it was right for me.
+
+            sorting the properties, I enrolled in numerous online web development courses, which included freeCodeCamp, Codeacademy and unity. I then enrolled at General Assembly as it seemed the perfect course and environment to start my journey as a Web Developer. I would love to continue my learning development in my next role.
+          </h2>
+        </div>
+      </div>
+    </section>
   );
 };
 
