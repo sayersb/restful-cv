f0ab2df03964f0e4e498a1db03ceca716fefdd0c
sayersb <bradleysayers1@gmail.com>
2018-08-28 18:12:05 +0100

    index.js updated errorhandler

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ app.use(bodyParser.json());
 app.use('/api', routes);
 
 app.use(express.static(`${__dirname}/public`));
-
+app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 app.use(errorHandler);
 
-app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
 
 app.listen(port, () => console.log(`Express running on port ${port}`));
 
