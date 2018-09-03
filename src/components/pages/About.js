ca213b96b9bcb96d0ad4e09e96ad32aa17aa52bc
sayersb <bradleysayers1@gmail.com>
2018-09-03 10:45:05 +0100

    skills and technologies updated to be centered, my github link added to skill link also

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -39,8 +39,11 @@ as a Web Developer. I would love to continue my learning development in my next 
             Skills
           </h1>
           <hr />
-          <h2 className="subtitle">
-            <strong>Some technologies implemented in projects so far :</strong>&nbsp;&nbsp; <a href="https://trello.com/"><i className="fab fa-trello fa-2x"></i>&nbsp;&nbsp;</a>
+          <h2 className="subtitle has-text-centered">
+            <strong>Some technologies implemented in projects so far</strong>
+          </h2>
+          <h2 className="subtitle has-text-centered">
+            &nbsp;&nbsp; <a href="https://trello.com/"><i className="fab fa-trello fa-2x"></i>&nbsp;&nbsp;</a>
             <a href="https://developer.paypal.com/docs/api/overview/"><i className="fab fa-cc-paypal fa-2x"></i>&nbsp;&nbsp;</a>
             <a href="https://mailchimp.com/"><i className="fab fa-mailchimp fa-2x"></i>&nbsp;&nbsp;</a>
             <a href="https://angular.io/"><i className="fab fa-angular fa-2x"></i>&nbsp;&nbsp;</a>
@@ -49,7 +52,7 @@ as a Web Developer. I would love to continue my learning development in my next 
             <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS3"><i className="fab fa-css3-alt fa-2x"></i>&nbsp;&nbsp;</a>
             <a href="https://sass-lang.com/"><i className="fab fa-sass fa-2x"></i>&nbsp;&nbsp;</a>
             <a href="https://en.wikipedia.org/wiki/HTML5"><i className="fab fa-html5 fa-2x"></i>&nbsp;&nbsp;</a>
-            <a href="https://github.com/"><i className="fab fa-github fa-2x"></i>&nbsp;&nbsp;</a>
+            <a href="https://github.com/sayersb"><i className="fab fa-github fa-2x"></i>&nbsp;&nbsp;</a>
             <a href="https://vuejs.org/"><i className="fab fa-vuejs fa-2x"></i></a>
           </h2>
         </div>
